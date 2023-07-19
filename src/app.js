3ae6ab006f586441ce3594a169e79544e93123c7
chin <a5566qq2581@gmail.com>
2023-07-19 14:54:36 +0800

    chore: use pino-pretty

--- src/app.js
+++ src/app.js
@@ -1,8 +1,30 @@
 
 import Fastify from "fastify";
 
+const envLogger = {
+    development: {
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                translateTime: 'HH:MM:ss Z',
+                ignore: 'pid,hostname',
+                colorize: true
+            },
+        },
+    },
+    production: {
+        transport: {
+            target: 'pino-pretty',
+            options: {
+                translateTime: 'HH:MM:ss Z',
+            }
+        },
+    },
+    test: false
+};
+
 const fastify = Fastify({
-    logger: true
+    logger: envLogger[process.env.NODE_ENV] ?? true,
 });
 
 export {
