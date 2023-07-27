cb796233e9a6d16c8fcfe7a3d6a32f070cd85a3f
chin <a5566qq2581@gmail.com>
2023-07-27 20:07:25 +0800

    refactor: move fastify instance into app.js
    - app.js is responsible for handling events related to the fastify instance
    - server.js is only responsible for starting the server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 
 import Fastify from "fastify";
+import { IncomingForm } from "formidable";
+import dotenv from "dotenv";
+import routesInit from "./routes/index.js";
+import * as R from "remeda";
+import path from "path";
+import { getCurrentDirname } from "./utils/currentModule.js";
+import cors from "@fastify/cors";
+
+dotenv.config();
 
 const envLogger = {
     development: {
@@ -26,6 +35,28 @@ const envLogger = {
 const fastify = Fastify({
     logger: envLogger[process.env.NODE_ENV] ?? true,
 });
+
+if (process.env.NODE_ENV !== "production") {
+    fastify.register(cors, {
+        origin: "*"
+    });
+}
+
+fastify.addContentTypeParser(["multipart/form-data"], async (req, payload) => {
+    let formParser = new IncomingForm({
+        uploadDir: path.join(getCurrentDirname(import.meta.url), "./temp-upload"),
+        multiples: true,
+        maxFileSize: 100 * 1024 * 1024 * 1024
+    });
+    const [fields, files] = await formParser.parse(payload);
+    let body = {};
+    body = R.addProp(body, "fields", fields);
+    body = R.merge(body, files);
+    
+    return body;
+});
+
+await routesInit();
 
 export {
     fastify as app
--- src/server.js
+++ src/server.js
@@ -9,28 +9,6 @@ import cors from "@fastify/cors";
 
 dotenv.config();
 
-if (process.env.NODE_ENV !== "production") {
-    app.register(cors, {
-        origin: "*"
-    });
-}
-
-app.addContentTypeParser(["multipart/form-data"], async (req, payload) => {
-    let formParser = new IncomingForm({
-        uploadDir: path.join(getCurrentDirname(import.meta.url), "./temp-upload"),
-        multiples: true,
-        maxFileSize: 100 * 1024 * 1024 * 1024
-    });
-    const [fields, files] = await formParser.parse(payload);
-    let body = {};
-    body = R.addProp(body, "fields", fields);
-    body = R.merge(body, files);
-    
-    return body;
-});
-
-await routesInit();
-
 app.listen({
     port: process.env.PORT,
     host: process.env.HOST
